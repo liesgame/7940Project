b474c9784e53e98cae3535926439f194dfa34046
liesgame <120369068@qq.com>
2023-04-08 01:19:27 +0800

    add stock

--- chatbot.py
+++ chatbot.py
@@ -5,6 +5,8 @@ import logging
 from kazoo.client import KazooClient, DataWatch
 import pika
 import time
+from datetime import datetime
+import requests 
 import json
 import random
 import argparse
@@ -18,6 +20,27 @@ def encode(string:str):
    return base64.b64encode(string.encode())
 def decode(b64_str):
    return base64.b64decode(b64_str).decode()
+
+def getStockData(ticker):
+    base_url = "https://financialmodelingprep.com/api/v3/quote/"
+    key = "977af0c2ae1c0f98a9dcbeb8516e75b9"
+    full_url = base_url + ticker + "?apikey=" + key
+    r = requests.get(full_url)
+    stock_data= r.json()
+    return stock_data
+def generateMessage(data):
+    symbol = data[0]['symbol']
+    price = data[0]["price"]
+    changesPercent = data[0]["changesPercentage"]
+    timestamp = data[0]['timestamp']
+    current = datetime.fromtimestamp(timestamp)
+    message = str(current)
+    message += "\n" + symbol 
+    message += "\n$" + str(price)
+    if(changesPercent < -2):
+        message += "\nWarning! Price drop more than 2%!"
+    return message
+
 class chatbot():
   def __init__(self, config, ip,is_master):
     self.ip = ip
@@ -126,6 +149,17 @@ class chatbot():
           logging.info("Context: "+str(context))
           self.send(id = update.effective_chat.id, message_content = update.message.text, method='echo')
           # context.bot.send_message(chat_id = update.effective_chat.id, text = reply_message)
+
+      def stock(update, context):
+          logging.info("Update: "+str(update))
+          logging.info("Context: "+str(context))
+          try:
+             msg = context.args[0]
+          except(IndexError, ValueError):
+             update.message.reply_text('Usage:/ stock <keyword>')
+             return
+          self.send(id = update.effective_chat.id, message_content = msg, method='stock')
+          # context.bot.send_message(chat_id = update.effective_chat.id, text = reply_message)
       def add(update: Update, context: CallbackContext) -> None:
           logging.info("Update: "+str(update))
           logging.info("Context: "+str(context))
@@ -186,6 +220,7 @@ class chatbot():
       dispatcher.add_handler(CommandHandler("hello", hello))
       dispatcher.add_handler(CommandHandler("chat", chat))
       dispatcher.add_handler(CommandHandler("node", node))
+      dispatcher.add_handler(CommandHandler("stock", stock))
       dispatcher.add_handler(echo_handler)
       
       # TO start the bot
@@ -243,6 +278,11 @@ class chatbot():
 
         def return_echo(bot, id, return_message):
            bot.send_message(chat_id=id, text=return_message)
+
+        def return_stock(bot, id, return_message):
+           real_time_stock = getStockData(return_message)
+           textMessage = generateMessage(real_time_stock)
+           bot.send_message(chat_id=id, text=textMessage)
         
         def return_add(bot, id, return_message):
           try:
@@ -261,6 +301,8 @@ class chatbot():
            return_add(bot=bot, id =content_body['id'], return_message = content_body['message'])
         elif content_body['method'] == 'chat':
            return_chat(bot=bot, id =content_body['id'], return_message = content_body['message'])
+        elif content_body['method'] == 'chat':
+           return_stock(bot=bot, id =content_body['id'], return_message = content_body['message'])
       
       channel.basic_consume(result.method.queue,callback,auto_ack = False)
       channel.start_consuming()
